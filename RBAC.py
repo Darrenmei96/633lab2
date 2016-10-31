512a12d6fe707a88d75fd60ec01b12bf7eed3580
raymonsobrepena <raymon.p.sobrepena@gmail.com>
2016-10-31 19:45:36 -0400

    Updated RBAC and fixed DAC file

--- RBAC.py
+++ RBAC.py
@@ -1,18 +1,15 @@
 #Question 3.2 
 
-roles = {0 : 'Manager', 1 : 'Technical Staff', 2 : 'Sales'}
+roles = { 'Manager':0 , 'Technical Staff': 1 ,'Sales' : 2}
 
 perm = {0: ' - ', 1: ' r ', 2: ' w ', 3 : ' rw', 4 : ' rx', 5 : 'rwx'}
-users = {'User1':0, 'User2':1, 'User3':2, 'User4':3,'User5':4}
+users = {'User1':roles['Manager'], 'User2':roles['Technical Staff'], 'User3':roles['Technical Staff'], 'User4':roles['Sales'],'User5':roles['Sales']}
 files = {0 : 'File1', 1 : 'File2', 2 : 'File3', 3 : 'File4', 4 : 'File5', 5 : 'File6'}
 matrix = [[5,5,5,5,5,5],
           [3,0,4,0,1,0],
-          [0,0,3,3,0,0],
-          [4,0,0,1,1,0],
-          [0,5,0,1,0,1]   
+          [0,0,3,3,0,0]
           ]
 
-class User:
 #Main function, runs appropriate functions 
 def main():
     print("Main Menu:\n1-Print Current User Role\n2-Change User Access\n3-Print Matrix")
@@ -24,11 +21,14 @@ def main():
     
 
 
+matrix[roles[user]][request]
 #Assign User a roles, if already has a role, changes it 
-def assign():
-    print("Assign Menu:\n1-Assign Role to different User \n2-Change Current Role")
-    option = int(input())
-    if option == 1:
+##def assign():
+##    print("Assign Menu:\n1-Assign Role to different User \n2-Change Current Role")
+##    option = int(input())
+##    if option == 1:
+        
+            
         
         
 
@@ -41,10 +41,9 @@ def printRole():
     
     
 
-def userCheck(user):
+##def userCheck(user):
     
 
-if __name__ == "__main__":
-          print("Enter your username")
-          user = int(input())
-          main()
+print("Enter your username")
+user = int(input())
+main()
