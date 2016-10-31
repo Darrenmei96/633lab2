1a8d5883e4b9e99da1950a07411561732d8d9275
raymonsobrepena <raymon.p.sobrepena@gmail.com>
2016-10-31 14:37:13 -0400

    Added main file and updated 3.2(not finished)

--- RBAC.py
+++ RBAC.py
@@ -1,8 +1,20 @@
 #Question 3.2 
 
+roles = {0 : 'Manager', 1 : 'Technical Staff', 2 : 'Sales'}
+
+        
+
+
+
+
+
 
 def main():
 
+
+
+
+def assign():
 
 
 
---
+++ main.py
@@ -0,0 +1,10 @@
+#Main File 
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
