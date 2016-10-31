03d5498a0621306644d8af6abacdbb5390e5f7a5
raymonsobrepena <raymon.p.sobrepena@gmail.com>
2016-10-30 22:43:12 -0400

    Added files for Q3

--- DAC.py
+++ DAC.py
@@ -1,1 +1,28 @@
 #Question 3
+
+permissions = {'-': 0, 'r': 1, 'w': 2, 'rw' : 3, 'rx' : 4, 'rwx' : 5}
+users = {'User1':0, 'User2':1, 'User3':2, 'User4':3,'User5':4}
+
+matrix = [[5,5,5,5,5,5],
+          [3,0,4,0,1,0],
+          [0,0,3,3,0,0],
+          [4,0,0,1,1,0],
+          [0,5,0,0,1,0,1]   
+          ]
+
+
+def main():
+    print(matrix[0][1])
+
+
+## def changePriv():
+
+
+##def printMatrix:
+##          for i 
+    
+
+if __name__ == "__main__":
+    main()
+
+    
