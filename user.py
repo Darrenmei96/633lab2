180a39a8d8e1c8db77d6f379dffaf5681faaa494
raymonsobrepena <raymon.p.sobrepena@gmail.com>
2016-10-31 18:01:19 -0400

    Added FAR/FRR

--- user.py
+++ user.py
@@ -8,6 +8,7 @@ class User:
         self.fly = []
         self.dwell1 = []
         self.dwell2 = []
+        self.threshold = 70
 
     def loadfile(self,filename):
         #read all the lines
@@ -55,12 +56,24 @@ def devs(flyrefs,flymons,dwellrefs,dwellmons):
     d = (dg/(n1-1) + (mg/n2))*50
     return d
 
+def rate(arr):
+	percent = 0.0
+	for i in range(0,len(arr)):
+	    percent += arr[i]
+	return percent / len(arr)
+
+	
 user = []
 for i in range (1,6):
     user.append(User())
     user[i-1].loadfile(dataloc + "User" + str(i) + ".txt" )
     print ("User" + str(i) + " has been loaded into memory")
 
+user[0].threshold = 80
+user[1].threshold = 78
+user[2].threshold = 75
+user[3].threshold = 75
+user[4].threshold = 78
 
 #user = User()
 #name = input("Enter in a username: ")
@@ -74,7 +87,7 @@ for i in range(0,5):
     for x in range (1,6):
         deviation = devs(user[i].fly[0:n],user[i].fly[x*n:x*n+n],user[i].dwell1[0:n],user[i].dwell1[x*n:x*n+n])
         tmp.append(deviation)
-        tmp2.append(fr(deviation,70))
+        tmp2.append(fr(deviation,user[i].threshold))
     deviations.append(tmp)
     falserejects.append(tmp2)
 
@@ -93,7 +106,7 @@ for i in range(0, 5):
         for y in range (1,6):
             deviation = devs(user[i].fly[0:n],user[x].fly[y*n:y*n+n],user[i].dwell1[0:n],user[x].dwell1[y*n:y*n+n])
             tmp3.append(deviation)
-            tmp4.append(fa(deviation,70))
+            tmp4.append(fa(deviation,user[i].threshold))
         tmp.append(tmp3)
         tmp2.append(tmp4)
     fadeviations.append(tmp)
@@ -103,10 +116,12 @@ for i in range(0,5):
     print("User "+ str(i+1))
     print("Deviations: ", deviations[i])
     print("False Rejects: ", falserejects[i])
+    print("False Reject Rate(Threshold",user[i].threshold, "): ", rate(falserejects[i])*100, "%")
     for j in range (0,4):
         usrno = j + 1
         if i == j:
             usrno += 1
         print("FA Deviations User "+str(usrno), ": ", fadeviations[i][j])
         print("False Accepts: ", falseaccepts[i][j])
+        print("False Accept Rate(Threshold",user[i].threshold, "): ", rate(falseaccepts[i][j])*100, "%")
     print("")
